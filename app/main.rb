5f3502b56921d71d37465909578b24dc5e05062c
mantas.kemesius <mantas.kemesius@gmail.com>
2018-09-30 19:22:24 +0300

    cleaning up main.rb

--- app/main.rb
+++ app/main.rb
@@ -2,12 +2,6 @@
 
 require './src/model/User.rb'
 require '../app/src/Helpers/files_handler.rb'
-require 'json'
-
-# user = User.new('petras', 'asfa', 1)
-
-# puts user.role
-# puts JSON.parse(File.read('./fakeDatabase/Users.json'))
 
 files_handler = FilesHandler.new(__dir__)
 
