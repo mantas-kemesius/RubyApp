e7561d7932155c33b465b53ecc8bd5741a1e1e58
mantas.kemesius <mantas.kemesius@gmail.com>
2018-09-30 19:46:56 +0300

    Add login controller, write some tests

--- app/main.rb
+++ app/main.rb
@@ -2,7 +2,11 @@
 
 require './src/model/User.rb'
 require '../app/src/Helpers/files_handler.rb'
+require '../app/src/controllers/login_controller.rb'
 
-files_handler = FilesHandler.new(__dir__)
+# files_handler = FilesHandler.new
+# puts files_handler.load_data('Users')
 
-puts files_handler.load_data('Users')
+
+# login_controller = LoginController.new
+# puts login_controller.login?('s1612345', '123')
--- app/src/controllers/login_controller.rb
+++ app/src/controllers/login_controller.rb
@@ -1,2 +1,19 @@
+require_relative './../Helpers/files_handler'
+
 class LoginController
+
+  def initialize
+    @file_handler = FilesHandler.new
+  end
+
+  def login?(username, password, file_name = 'Users')
+    users = @file_handler.load_data(file_name)
+    users['Users'].each do |item|
+      if item['username'] == username && item['password'] == password
+        return true
+      end
+    end
+    false
+  end
+
 end
