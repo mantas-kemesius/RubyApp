61c8a76754e15aa74a120962f8362e3e8ce0849f
mantas.kemesius <mantas.kemesius@gmail.com>
2018-10-01 20:26:55 +0300

    MK-1: add hardcoded user credentials

--- app/main.rb
+++ app/main.rb
@@ -4,9 +4,17 @@ require './src/model/User.rb'
 require '../app/src/helpers/files_handler.rb'
 require '../app/src/controllers/login_controller.rb'
 
-# files_handler = FilesHandler.new
-# puts files_handler.load_data('Users')
+# student credentials
+username = 's1612346'
+password = '123'
+
+# lecturer credentials
+username = 's1612345'
+password = '123'
 
 
-# login_controller = LoginController.new
-# puts login_controller.login?('s1612345', '123')
+login_controller = LoginController.new
+puts login_controller.login?(username, password)
+
+# while(true)
+# end
