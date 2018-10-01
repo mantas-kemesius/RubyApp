fc3d882cd8a1bf54fdeba2f43e82ac870bb3d1dd
mantas.kemesius <mantas.kemesius@gmail.com>
2018-10-01 21:50:38 +0300

    MK-1: add file_handler tests

--- app/tests/files_handler_tests.rb
+++ app/tests/files_handler_tests.rb
@@ -3,7 +3,23 @@ require_relative './../src/helpers/files_handler'
 require 'json'
 RSpec.describe FilesHandler do
   file_handler = FilesHandler.new
-  it 'Data from file should be: test' do
-    expect(file_handler.load_data('testFiles/Test')['test']).to eq 'test'
+  data = ['data' => [1, 2, 3]]
+  file_name = 'testFiles/testCreate'
+  it 'File was created successfully' do
+    file_handler.save_data(
+        data,
+      'testFiles/testCreate'
+    )
+    expect(file_handler.file_exist?(file_name)).to eq true
   end
+
+  it 'File has right content' do
+    expect(file_handler.load_data(file_name)).to eq data
+  end
+
+  it 'File was deleted successfully' do
+    file_handler.delete_file(file_name)
+    expect(file_handler.file_exist?(file_name)).to eq false
+  end
+
 end
