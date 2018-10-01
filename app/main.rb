0d5f9b7fb1a613acfab0e9b399800314b5ef43b2
mantas.kemesius <mantas.kemesius@gmail.com>
2018-10-01 20:47:28 +0300

    MK-1: add console app

--- app/main.rb
+++ app/main.rb
@@ -4,6 +4,15 @@ require './src/model/User.rb'
 require '../app/src/helpers/files_handler.rb'
 require '../app/src/controllers/login_controller.rb'
 
+def menu
+  puts '1. Show pazymiai'
+  puts '2. Show tvarkarasciai'
+  puts '3. Show user info'
+  puts '4. Show something more'
+  puts 'exit: To exit from program'
+  puts puts
+end
+
 # student credentials
 username = 's1612346'
 password = '123'
@@ -12,9 +21,23 @@ password = '123'
 username = 's1612345'
 password = '123'
 
-
 login_controller = LoginController.new
 puts login_controller.login?(username, password)
 
-# while(true)
-# end
+loop do
+  menu
+  print 'Choose: '
+  input = gets.chomp
+  case input
+  when '1'
+    puts input
+  when '2'
+    puts input
+  when '3'
+    puts input
+  when '4'
+    puts input
+  when 'exit'
+    break
+  end
+end
