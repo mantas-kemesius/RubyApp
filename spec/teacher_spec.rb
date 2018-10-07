69744244dc78d69d2003387ea3efd5d64a7724b2
TomasMacikas <macikastomas@gmail.com>
2018-10-07 14:27:21 +0300

    Added Teacher, teacher_spec

--- spec/teacher_spec.rb
+++ spec/teacher_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 RSpec.describe Teacher do
-  teacher = described_class.new("Vilius", "Vilniaus universitetas", "mif")
+  teacher = described_class.new('Vilius', 'Vilniaus universitetas', 'mif')
   it 'have name' do
     expect(teacher.name).to eq 'Vilius'
     expect(teacher.university).to eq 'Vilniaus universitetas'
