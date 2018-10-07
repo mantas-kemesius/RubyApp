292b8be6433aab1b39ba84b350c5f3ebdb272e28
TomasMacikas <macikastomas@gmail.com>
2018-10-07 14:24:24 +0300

    Added Teacher, teacher_spec

--- lib/model/teacher.rb
+++ lib/model/teacher.rb
@@ -1,7 +1,9 @@
 class Teacher
-  attr_accessor :name
+  attr_accessor :name, :faculty, :university
 
-  def initialize
-    @name = 'Tomas'
+  def initialize(name, university, faculty)
+    @name = name
+    @university = university
+    @faculty = faculty
   end
 end
--- spec/teacher_spec.rb
+++ spec/teacher_spec.rb
@@ -1,8 +1,10 @@
 require 'spec_helper'
 
 RSpec.describe Teacher do
-  teacher = described_class.new
+  teacher = described_class.new("Vilius", "Vilniaus universitetas", "mif")
   it 'have name' do
-    expect(teacher.name).to eq 'Tomas'
+    expect(teacher.name).to eq 'Vilius'
+    expect(teacher.university).to eq 'Vilniaus universitetas'
+    expect(teacher.faculty).to eq 'mif'
   end
 end
