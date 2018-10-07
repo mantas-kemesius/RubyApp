a3e753e7b66a3607f9c5d464e8f1a01f3feda28c
arnasRad <arnasrad@gmail.com>
2018-10-07 17:30:10 +0300

    Added User class getters / setters, rspec tests in user_spec.rb

--- lib/model/user.rb
+++ lib/model/user.rb
@@ -8,4 +8,45 @@ class User
     @email = email
     @phone = phone
   end
+
+  def getName
+    return @name
+  end
+
+  def setName (name)
+    @name = name
+  end
+
+  def getLastName
+    return @last_name
+  end
+
+  def setLastName (last_name)
+    @last_name = last_name
+  end
+
+  def getRoleID
+    return @role
+  end
+
+  def setRoleID (role_id)
+    @role = ROLE[role_id]
+  end
+
+  def getEmail
+    return @email
+  end
+
+  def setEmail (email)
+    @email = email
+  end
+
+  def getPhone
+    return @phone
+  end
+
+  def setPhone (phone)
+    @phone = phone
+  end
+
 end
