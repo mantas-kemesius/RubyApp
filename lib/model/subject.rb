ffd263d680f48cf1a13b76a43b065307e31b0bfa
mantas.kemesius <mantas.kemesius@gmail.com>
2018-10-08 22:58:44 +0300

    improvements

--- lib/model/subject.rb
+++ lib/model/subject.rb
@@ -1,5 +1,5 @@
-# Attributes of a study subject
 require_relative 'subject_info'
+# Attributes of a study subject
 class Subject < SubjectInfo
   attr_reader :name, :description_text, :description_file
   def initialize(name, description_text, description_file, number_of_credits, coordinating_teacher)
