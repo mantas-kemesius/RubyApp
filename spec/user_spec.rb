5de6b5b88af1f2a2a3f08e76d979dd7524675506
mantas.kemesius <mantas.kemesius@gmail.com>
2018-10-08 20:38:09 +0300

    remove some tests

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -28,32 +28,3 @@ RSpec.describe User do
     expect(user.phone).to eq ''
   end
 end
-
-RSpec.describe User do
-  context 'to test getters and setters' do
-    it 'getters are working correctly' do
-      temp_user = User.new('Vardenis', 'Pavardenis', 0,
-                           'vardenis.pavardenis@example.com', '+37051234567')
-      expect(temp_user.get_name).to eq 'Vardenis'
-      expect(temp_user.get_last_name).to eq 'Pavardenis'
-      expect(temp_user.get_role_id).to eq 'ROLE_STUDENT'
-      expect(temp_user.get_email).to eq 'vardenis.pavardenis@example.com'
-      expect(temp_user.get_phone).to eq '+37051234567'
-    end
-
-    it 'setters are working correctly' do
-      temp_user = User.new('', '', 0)
-      temp_user.set_name('Vardenis')
-      temp_user.set_last_name('Pavardenis')
-      temp_user.set_role_id(0)
-      temp_user.set_email('vardenis.pavardenis@example.com')
-      temp_user.set_phone('+37051234567')
-
-      expect(temp_user.get_name).to eq 'Vardenis'
-      expect(temp_user.get_last_name).to eq 'Pavardenis'
-      expect(temp_user.get_role_id).to eq 'ROLE_STUDENT'
-      expect(temp_user.get_email).to eq 'vardenis.pavardenis@example.com'
-      expect(temp_user.get_phone).to eq '+37051234567'
-    end
-  end
-end
