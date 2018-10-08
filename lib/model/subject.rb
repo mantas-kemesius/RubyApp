4841376c30258c5dcd0fb995a3aecc45908d353b
arnasRad <arnasrad@gmail.com>
2018-10-09 00:03:37 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	lib/model/user.rb

--- lib/model/subject.rb
+++ lib/model/subject.rb
@@ -2,7 +2,8 @@ require_relative 'subject_info'
 # Attributes of a study subject
 class Subject < SubjectInfo
   attr_reader :name, :description_text, :description_file
-  def initialize(name, description_text, description_file, number_of_credits, coordinating_teacher)
+  def initialize(name, description_text, description_file,
+                 number_of_credits, coordinating_teacher)
     super(number_of_credits, coordinating_teacher)
     @name = name
     @description_text = description_text
