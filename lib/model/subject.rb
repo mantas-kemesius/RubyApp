e5432e4b1d613890c9814f253dffbae0910345ba
Juozas <43491708+JuozasN@users.noreply.github.com>
2018-10-08 22:37:52 +0300

    subject info subclass

--- lib/model/subject.rb
+++ lib/model/subject.rb
@@ -1,13 +1,11 @@
 # Attributes of a study subject
-class Subject
-  attr_reader :name, :description_text, :description_file, :number_of_credits,
-                :coordinating_teacher
+class Subject < SubjectInfo
+  attr_reader :name, :description_text, :description_file
   def initialize(name, description_text, description_file, number_of_credits,
                  coordinating_teacher)
+    super(number_of_credits, coordinating_teacher)
     @name = name
     @description_text = description_text
     @description_file = description_file
-    @number_of_credits = number_of_credits
-    @coordinating_teacher = coordinating_teacher
   end
 end
