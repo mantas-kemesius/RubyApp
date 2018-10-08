50e9e5a798ae15a55c6c28840bda6216ecab5dae
TomasMacikas <macikastomas@gmail.com>
2018-10-08 22:09:58 +0300

    reek update

--- spec/login_controller_spec.rb
+++ spec/login_controller_spec.rb
@@ -13,27 +13,27 @@ user = {
 
 RSpec.describe(LoginController) do
   it 'Login should be unsuccessful' do
-    login_controller = LoginController.new
+    login_controller = described_class.new
     expect(
       login_controller.login?('s1612345', '1223', 'testFiles/Users')
     ).to eq false
   end
 
   it 'Login should be successful' do
-    login_controller = LoginController.new
+    login_controller = described_class.new
     expect(login_controller
        .login?('s1612345', '123', 'testFiles/Users')).to eq true
   end
 
   it 'Returned object should be created from User class' do
-    login_controller = LoginController.new
+    login_controller = described_class.new
     login_controller.login?('s1612345', '123', 'testFiles/Users')
     expect(login_controller.user_obj.class).to eq User
   end
 end
 RSpec.describe(LoginController) do
   # Init serializer
-  login_controller = LoginController.new
+  login_controller = described_class.new
   login_controller.serializer(user)
 
   it 'Serializer makes user object from user array' do
