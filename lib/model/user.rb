58106ed268e68f5320132de990707609ad867399
mantas.kemesius <mantas.kemesius@gmail.com>
2018-10-08 23:45:00 +0300

    resolve tests

--- lib/model/user.rb
+++ lib/model/user.rb
@@ -1,7 +1,7 @@
 # User class
 class User
   ROLE = %w[ROLE_STUDENT ROLE_LECTURER ROLE_ADMIN].freeze
-  attr_reader :name, :last_name, :role, :email, :phone, :user_obj
+  attr_reader :name, :last_name, :role, :email, :phone
   def initialize(name, last_name, role_id, email = '', phone = '')
     @name = name
     @last_name = last_name
@@ -12,15 +12,5 @@ class User
                ''
              end
     @phone = phone
-  end
-
-  def serializer(user)
-    @user_obj = User.new(
-        user['name'],
-        user['lastName'],
-        user['role_id'],
-        user['email'],
-        user['phone']
-    )
   end
 end
