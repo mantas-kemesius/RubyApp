ce6ac3be957030530670aff9998e9f7897be38cd
TomasMacikas <macikastomas@gmail.com>
2018-10-09 13:11:40 +0300

    update on run.sh and few others

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 bundle install
-bundle exec mutant --include lib --use rspec main
+bundle exec mutant --include lib --use rspec Teacher User Email Student Subject Serializer
 bundle exec rspec
 bundle exec rubocop
 bundle exec reek
