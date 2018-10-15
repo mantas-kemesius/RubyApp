d51a74ca469b753054405e186dbf4bfa26d2d326
mEimantas <43536171+mEimantas@users.noreply.github.com>
2018-10-15 17:12:30 +0300

    mutant fixes

--- lib/model/student.rb
+++ lib/model/student.rb
@@ -1,24 +1,23 @@
-require_relative 'student_info'
 # Description/Explanation of Student class
-class Student < StudentInfo
-  attr_reader :s_id, :faculty, :group, :active
-  def initialize(s_id, faculty, group)
-    @s_id = s_id
-    @faculty = faculty
-    @group = group
-    @active = 1
-    super()
-  end
+class Student
+  attr_reader :s_id, :group, :faculty, :study_program
 
-  def add_subject(subject)
-    subjects.push(subject)
+  def initialize(s_id, group, faculty, study_program)
+    @s_id = s_id if s_id.positive?
+    @group = group if group.positive?
+    @faculty = faculty
+    @study_program = study_program
   end
 
   def change_group(new_group)
-    @group = new_group
+    @group = new_group if new_group.positive?
   end
 
-  def make_student_activity(new_active)
-    @active = new_active
+  def change_faculty(new_faculty)
+    @faculty = new_faculty
+  end
+
+  def change_study_program(new_study_program)
+    @study_program = new_study_program
   end
 end
