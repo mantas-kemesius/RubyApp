486e4ae6f3bbbf3b7271c9cf96c8039b7eb18569
mEimantas <43536171+mEimantas@users.noreply.github.com>
2018-10-15 21:50:34 +0300

    mutant 95

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,65 +1,70 @@
 require 'spec_helper.rb'
 
-RSpec.describe User do
-  user = described_class.new('Petras', 'Petraitis', 0)
-  temp_user = described_class.new('Petras', 'Petraitis', 0,
-                                  'email@email.com', '8623423423')
-  it 'user name was setted successfuly' do
-    expect(user.name).to eq 'Petras'
-  end
+describe User do
+  context 'when created' do
+    let(:user) { described_class.new('Petras', 'Petraitis', 0)}
+    let(:temp_user) { described_class.new('Petras', 'Petraitis', 0,
+                                  'email@email.com', '8623423423')}
+    it 'user name was setted successfuly' do
+      expect(user.name).to eq 'Petras'
+    end
 
-  it 'user lastname was set successfuly' do
-    expect(user.last_name).to eq 'Petraitis'
-  end
+    it 'user lastname was set successfuly' do
+      expect(user.last_name).to eq 'Petraitis'
+    end
 
-  it 'user role was set successfuly' do
-    expect(user.role).to eq 'ROLE_STUDENT'
-  end
+    it 'user role was set successfuly' do
+      expect(user.role).to eq 'ROLE_STUDENT'
+    end
 
-  it 'user email was set empty successfuly' do
-    expect(user.email).to eq ''
-  end
+    it 'user email was set empty successfuly' do
+      expect(user.email).to eq ''
+    end
 
-  it 'user phone was set empty successfuly' do
-    expect(user.phone).to eq ''
-  end
+    it 'user phone was set empty successfuly' do
+      expect(user.phone).to eq ''
+    end
 
-  it 'user email was set successfuly' do
-    expect(temp_user.email).to eq 'email@email.com'
-  end
+    it 'user email was set successfuly' do
+      expect(temp_user.email).to eq 'email@email.com'
+    end
 
-  it 'user phone was set successfuly' do
-    expect(temp_user.phone).to eq '8623423423'
+    it 'user phone was set successfuly' do
+     expect(temp_user.phone).to eq '8623423423'
+    end
   end
 end
 
 # run all tests at one time
-RSpec.describe User do
-  file_handler = FilesHandler.new
+describe User do
+  context 'when created' do
+  let(:file_handler) { FilesHandler.new}
   user_email = 'petras@gmail.com'
   email_file_name = 'testFiles\Emails'
-  user = described_class.new('Petras', 'Petraitis', 0, user_email)
+  let(:user) { described_class.new('Petras', 'Petraitis', 0, user_email)}
+  
 
-  it 'sends email successfully' do
-    # create temporary data file
-    file_handler.save_data([], email_file_name)
-    sent_successfully = user.send_email('jonas@gmail.com', 'Test email',
+    it 'sends email successfully' do
+     # create temporary data file
+      file_handler.save_data([], email_file_name)
+      sent_successfully = user.send_email('jonas@gmail.com', 'Test email',
                                         'testing email functionality',
                                         email_file_name)
-    expect(sent_successfully).to eq true
-  end
+      expect(sent_successfully).to eq true
+    end
 
-  it 'gets user emails successfully' do
-    email_arr = user.sent_emails(email_file_name)
-    expect(email_arr[0].email_from).to eq user_email
-  end
+    it 'gets user emails successfully' do
+      email_arr = user.sent_emails(email_file_name)
+      expect(email_arr[0].email_from).to eq user_email
+    end
 
-  it 'gets sent email count successfully' do
-    expect(user.sent_email_count(email_file_name)).to eq 1
-  end
+    it 'gets sent email count successfully' do
+      expect(user.sent_email_count(email_file_name)).to eq 1
+    end
 
-  it 'gets received emails successfully' do
-    expect(user.received_email_count(email_file_name)).to eq 0
-    file_handler.delete_file(email_file_name)
+    it 'gets received emails successfully' do
+      expect(user.received_email_count(email_file_name)).to eq 0
+      file_handler.delete_file(email_file_name)
+    end
   end
 end
