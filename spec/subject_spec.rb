36b635f8d547d29f095c28dddc73825c5d0e8b21
Juozas <43491708+JuozasN@users.noreply.github.com>
2018-10-15 22:09:07 +0300

    subject info subclass

--- spec/subject_spec.rb
+++ spec/subject_spec.rb
@@ -1,8 +1,10 @@
 require 'spec_helper'
 describe Subject do
   context 'when created' do
-    let(:subject) { described_class.new('Ruby Programming', 'Agile Programming Ruby',
-                                        'Ruby.pdf', '5', 't1610179') }
+    let(:subject) do
+      described_class.new('Ruby Programming', 'Agile Programming Ruby',
+                          'Ruby.pdf', '5', 't1610179')
+    end
     it 'Subject name set correct' do
       expect(subject.name).to eq 'Ruby Programming'
     end
@@ -20,22 +22,23 @@ describe Subject do
       regex = /^(3|4|5|6|7|8|9|10|11|12|13|14|15){1}$/
       expect(!subject.number_of_credits[regex].nil?).to eq true
     end
-    it 'Credit number set correct' do
-      expect(subject.number_of_credits).to eq '5'
-    end
   end
 end
-
 describe Subject do
   context 'when created' do
-    let(:subject) { described_class.new('Ruby Programming', 'Agile Programming Ruby',
-                                        'Ruby.pdf', '5', 't1610179') }
+    let(:subject) do
+      described_class.new('Ruby Programming', 'Agile Programming Ruby',
+                          'Ruby.pdf', '5', 't1610179')
+    end
     it 'Coordinating teacher ID correct format' do
       regex = /[t]\d{7}$/
       expect(!subject.coordinating_teacher[regex].nil?).to eq true
     end
     it 'Coordinating teacher set correct' do
       expect(subject.coordinating_teacher).to eq 't1610179'
+    end
+    it 'Credit number set correct' do
+      expect(subject.number_of_credits).to eq '5'
     end
   end
 end
