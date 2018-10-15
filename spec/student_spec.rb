347e0912ac90464362cb6bd51a5856bcee12b8c8
mEimantas <43536171+mEimantas@users.noreply.github.com>
2018-10-15 17:11:28 +0300

    mutant fixes

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -1,63 +1,56 @@
 require 'spec_helper.rb'
-RSpec.describe Student do
-  student = described_class.new('s1611123', 'MIF', 2)
+describe Student do
+  context 'when created' do
+    let(:student) { described_class.new(123, 10, 'MIF', 'Informatika') }
+    let(:student_bad) { described_class.new(-123, -10, 'MIF', 'Informatika') }
 
-  it 'initial student data was set successful' do
-    expect(student.s_id).to eq 's1611123'
-    expect(student.faculty).to eq 'MIF'
-    expect(student.group).to eq 2
-    expect(student.subjects).to eq []
-  end
+    it 'initial student number was set successful' do
+      expect(student.s_id).to eq 123
+    end
 
-  it 'initial student data was set unsuccessful' do
-    expect(student.s_id).not_to eq 'S1112531'
-    expect(student.faculty).not_to eq 'MIT'
-    expect(student.group).not_to eq '3'
-    expect(student.subjects).not_to eq [['Matematika']]
-  end
-end
+    it 'initial student group was set successful' do
+      expect(student.group).to eq 10
+    end
 
-RSpec.describe Student do
-  student = described_class.new('s1611123', 'MIF', 2)
-  it 'adding subject was successful' do
-    student.add_subject('Anglu')
-    expect(student.subjects.last).to eq 'Anglu'
-  end
+    it 'initial student faculty was set successful' do
+      expect(student.faculty).to eq 'MIF'
+    end
 
-  it 'adding subject was unsuccessful' do
-    student.add_subject('Anglu')
-    expect(student.subjects.last).not_to eq 'Matematika'
-  end
+    it 'initial student number was set unsuccessful' do
+      expect(student_bad.s_id).to eq nil
+    end
 
-  it 'changing group was successful' do
-    student.change_group(3)
-    expect(student.group).to eq 3
+    it 'initial student group was set unsuccessful' do
+      expect(student_bad.group).to eq nil
+    end
   end
 end
-RSpec.describe Student do
-  student = described_class.new('s1611123', 'MIF', 2)
-  it 'changing group was unsuccessful' do
-    student.change_group(3)
-    expect(student.group).not_to eq 4
-  end
+describe Student do
+  context 'when changing' do
+    let(:student_group) { described_class.new(123, 5, 'MIF', 'Informatika') }
 
-  it 'changing student state to active was successful' do
-    student.make_student_activity(1)
-    expect(student.active).to eq 1
-  end
+    it ' student group change was successful' do
+      student_group.change_group(6)
+      expect(student_group.group).to eq 6
+    end
 
-  it 'changing student state to active was unsuccessful' do
-    student.make_student_activity(1)
-    expect(student.active).not_to eq 0
-  end
+    it ' student group change unsuccessful' do
+      student_group.change_group(-15)
+      expect(student_group.group).to eq 5
+    end
 
-  it 'changing student state to unactive was successful' do
-    student.make_student_activity(0)
-    expect(student.active).to eq 0
-  end
+    it ' student change_faculty change successful' do
+      student_group.change_faculty('FF')
+      expect(student_group.faculty).to eq 'FF'
+    end
 
-  it 'changing student state to unactive was unsuccessful' do
-    student.make_student_activity(0)
-    expect(student.active).not_to eq 1
+    it 'initial student study_program was set successful' do
+      expect(student_group.study_program).to eq 'Informatika'
+    end
+
+    it ' student study_program change was successful' do
+      student_group.change_study_program('PS')
+      expect(student_group.study_program).to eq 'PS'
+    end
   end
 end
