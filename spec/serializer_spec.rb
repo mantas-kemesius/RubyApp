4dc094ddfa91217fec4bef7ce1d12dca2049e6df
Juozas <43491708+JuozasN@users.noreply.github.com>
2018-10-15 22:25:42 +0300

    fixing rubocop

--- spec/serializer_spec.rb
+++ spec/serializer_spec.rb
@@ -10,25 +10,39 @@ describe Serializer do
       'phone' => '862435384',
       'password' => '123'
     }
-     # Init serializer
-    let(:serializer) {described_class.new}
-    let(:user_obj) {serializer.serializer(user_array)}
-
+    # Init serializer
+    let(:serializer) { described_class.new }
+    let(:user_obj) { serializer.serializer(user_array) }
     it 'Serializer makes user object from user array' do
-     expect(user_obj.class).to eq User
+      expect(user_obj.class).to eq User
     end
-
     it 'Serializer creates name from user array' do
       expect(user_obj.name).to eq user_array['name']
     end
     it 'Serializer creates last name from user array' do
-     expect(user_obj.last_name).to eq user_array['lastName']
+      expect(user_obj.last_name).to eq user_array['lastName']
     end
+    # it 'Serializer creates email from user array' do
+    # expect(user_obj.email).to eq user_array['email']
+  end
+end
+describe Serializer do
+  context 'when created' do
+    user_array = {
+      'username' => 's1612345',
+      'name' => 'Petras',
+      'lastName' => 'Petraitis',
+      'role' => 'ROLE_LECTURER',
+      'role_id' => 1,
+      'email' => 'petras@gmail.com',
+      'phone' => '862435384',
+      'password' => '123'
+    }
+    # Init serializer
+    let(:serializer) { described_class.new }
+    let(:user_obj) { serializer.serializer(user_array) }
     it 'Serializer creates phone from user array' do
       expect(user_obj.phone).to eq user_array['phone']
     end
-  # it 'Serializer creates email from user array' do
-  # expect(user_obj.email).to eq user_array['email']
-  # end
   end
 end
