c06c1acbe22059a75039671c7c5c99f56940c12e
mEimantas <43536171+mEimantas@users.noreply.github.com>
2018-10-23 17:42:48 +0300

    small fix

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -7,29 +7,18 @@ describe User do
       described_class.new('Petras', 'Petraitis', 0,
                           'email@email.com', '8623423423')
     end
+
     it 'user name was setted successfuly' do
       expect(user.name).to eq 'Petras'
     end
     it 'user lastname was set successfuly' do
       expect(user.last_name).to eq 'Petraitis'
     end
-    it 'user role was set successfuly' do
-      expect(user.role).to eq 'ROLE_STUDENT'
-    end
     it 'user email was set empty successfuly' do
       expect(user.email).to eq ''
     end
     it 'user phone was set empty successfuly' do
       expect(user.phone).to eq ''
-    end
-  end
-end
-describe User do
-  context 'when created' do
-    let(:user) { described_class.new('Petras', 'Petraitis', 0) }
-    let(:temp_user) do
-      described_class.new('Petras', 'Petraitis', 0,
-                          'email@email.com', '8623423423')
     end
     it 'user email was set successfuly' do
       expect(temp_user.email).to eq 'email@email.com'
