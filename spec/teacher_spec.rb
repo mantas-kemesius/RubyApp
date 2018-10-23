73c50ec85868772fa0427583d47db785f229a085
mEimantas <43536171+mEimantas@users.noreply.github.com>
2018-10-23 17:49:22 +0300

    describe

--- spec/teacher_spec.rb
+++ spec/teacher_spec.rb
@@ -4,13 +4,14 @@ describe Teacher do
     let(:teacher) do
       described_class.new('t1610179', 'Vilniaus universitetas', 'mif')
     end
+
     it 'ID set correct' do
       expect(teacher.id).to eq 't1610179'
     end
     it 'university set correct' do
       expect(teacher.university).to eq 'Vilniaus universitetas'
     end
-    it 'faculty set correct' do
+    it 'faculty was set correct' do
       expect(teacher.faculty).to eq 'mif'
     end
     it 'faculty set correct' do
@@ -23,30 +24,6 @@ describe Teacher do
     it 'teacher is on vacation' do
       teacher.goes_on_vacation(true)
       expect(teacher.on_vacation).to eq true
-    end
-  end
-end
-
-describe Teacher do
-  context 'when created' do
-    let(:teacher) do
-      described_class.new('t1610179', 'Vilniaus universitetas', 'mif')
-    end
-    it 'has no subjects' do
-      expect(teacher.subjects).to eq []
-    end
-    it 'can enter grade for student' do
-      expect(teacher.enter_grade?).to eq true
-    end
-    it 'can edit grade for student' do
-      expect(teacher.edit_grade?).to eq true
-    end
-    it 'can delete grade for student' do
-      expect(teacher.delete_grade?).to eq true
-    end
-    it 'teacher is on vacation' do
-      teacher.goes_on_vacation(false)
-      expect(teacher.on_vacation).to eq false
     end
   end
 end
