0bb12d39448666144dc2a54c8182315112a40c48
mEimantas <43536171+mEimantas@users.noreply.github.com>
2018-10-23 18:05:36 +0300

    describe

--- spec/subject_spec.rb
+++ spec/subject_spec.rb
@@ -1,44 +1,27 @@
 require 'spec_helper'
 describe Subject do
   context 'when created' do
-    let(:subject) do
+    let(:temp_subject) do
       described_class.new('Ruby Programming', 'Agile Programming Ruby',
                           'Ruby.pdf', '5', 't1610179')
     end
+
     it 'Subject name set correct' do
-      expect(subject.name).to eq 'Ruby Programming'
+      expect(temp_subject.name).to eq 'Ruby Programming'
     end
     it 'Subject description set correct' do
-      expect(subject.description_text).to eq 'Agile Programming Ruby'
+      expect(temp_subject.description_text).to eq 'Agile Programming Ruby'
     end
     it 'Subject description file correct format' do
       regex = /\w+[.][p][d][f]$/
-      expect(!subject.description_file[regex].nil?).to eq true
+      expect(!temp_subject.description_file[regex].nil?).to eq true
     end
     it 'Subject description file set correct' do
-      expect(subject.description_file).to eq 'Ruby.pdf'
+      expect(temp_subject.description_file).to eq 'Ruby.pdf'
     end
     it 'Credit number correct format' do
       regex = /^(3|4|5|6|7|8|9|10|11|12|13|14|15){1}$/
-      expect(!subject.number_of_credits[regex].nil?).to eq true
-    end
-  end
-end
-describe Subject do
-  context 'when created' do
-    let(:subject) do
-      described_class.new('Ruby Programming', 'Agile Programming Ruby',
-                          'Ruby.pdf', '5', 't1610179')
-    end
-    it 'Coordinating teacher ID correct format' do
-      regex = /[t]\d{7}$/
-      expect(!subject.coordinating_teacher[regex].nil?).to eq true
-    end
-    it 'Coordinating teacher set correct' do
-      expect(subject.coordinating_teacher).to eq 't1610179'
-    end
-    it 'Credit number set correct' do
-      expect(subject.number_of_credits).to eq '5'
+      expect(!temp_subject.number_of_credits[regex].nil?).to eq true
     end
   end
 end
