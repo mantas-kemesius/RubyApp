f207282cc6f542d4434bbec3f21794bd34bbbbc2
tomasmacikas <macikastomas@gmail.com>
2018-10-28 15:24:30 +0200

    notification initialize tests

--- lib/model/notification.rb
+++ lib/model/notification.rb
@@ -1,2 +1,10 @@
-class Notification
+require_relative 'text'
+# Notification class for all users
+class Notification < Text
+  attr_reader :date, :sender
+  def initialize(date, title, text, sender)
+    super(title, text)
+    @date = date
+    @sender = sender
+  end
 end
--- spec/notification_spec.rb
+++ spec/notification_spec.rb
@@ -1,7 +1,22 @@
 require 'spec_helper'
 
 RSpec.describe(Notification) do
-  let(:notifation) do
-    described_class.new
+  context 'on create' do
+    let(:notification) do
+      described_class.new('2018-28-10', 'First', 'Hello', 'Admin')
+    end
+
+    it 'date set correctly' do
+      expect(notification.date).to eq '2018-28-10'
+    end
+    it 'title set correctly' do
+      expect(notification.title).to eq 'First'
+    end
+    it 'text set correctly' do
+      expect(notification.text).to eq 'Hello'
+    end
+    it 'sender set correctly' do
+      expect(notification.sender).to eq 'Admin'
+    end
   end
 end
