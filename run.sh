b73fc4f3b667c67b08da8fe905ad7902ed4237fb
mantas.kemesius <mantas.kemesius@gmail.com>
2018-10-28 13:24:14 +0200

    add rubocop.yml file

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+cp rubocop.dist.yml .rubocop.yml
 bundle install
 bundle exec mutant --include lib --use rspec LoginController Teacher Subject Student User Email EmailInfo SubjectInfo
 bundle exec rspec
