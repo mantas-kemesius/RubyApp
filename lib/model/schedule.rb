30196395111930e454ba713d4b042f770cc627de
juozas <j.natkevicius@gmail.com>
2018-10-29 23:12:42 +0200

    Schedule class updates

--- lib/model/schedule.rb
+++ lib/model/schedule.rb
@@ -1,11 +1,13 @@
-require_relative './schedule_info'
+# require_relative './schedule_info'
 # Study Schedule class
-class Schedule < ScheduleInfo
-  attr_reader :date_day, :date_time
-  def initialize(date_day, date_time, class_number, subject_name, teacher_id)
-    @date_day = date_day
-    @date_time = date_time
-    super(class_number, subject_name, teacher_id)
-    @file_handler = FilesHandler.new('fakeDatabase/Schedule.json')
+class Schedule #  < ScheduleInfo
+  attr_reader :date_day, :date_time, :class_number, :subject_name, :teacher_id
+  def initialize(date, class_number, info)
+    @date_day = date.fetch('day')
+    @date_time = date.fetch('time')
+    @class_number = class_number
+    @subject_name = info.fetch('subject')
+    @teacher_id = info.fetch('teacher')
+    # @file_handler = FilesHandler.new('fakeDatabase/Schedule.json')
   end
 end
