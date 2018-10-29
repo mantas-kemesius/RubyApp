701e07100cfae843c311a676f5262b6fee515430
juozas <j.natkevicius@gmail.com>
2018-10-30 01:30:45 +0200

    added grades and exams classes with basic tests

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 bundle install
-bundle exec mutant --include lib --use rspec LoginController Teacher Subject Student User Email EmailInfo SubjectInfo FilesHandler ScheduleInfo Schedule
+bundle exec mutant --include lib --use rspec LoginController Teacher Subject Student User Email EmailInfo SubjectInfo FilesHandler Schedule Exams Grades
 bundle exec rspec
 bundle exec rubocop
 bundle exec reek
