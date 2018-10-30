3a1191b4876311d5abf158e5fb50a7f35b4282c6
mEimantas <43536171+mEimantas@users.noreply.github.com>
2018-10-30 12:43:33 +0200

    spec update

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -1,11 +1,43 @@
 require 'spec_helper'
 require 'spec_helper.rb'
+require_relative '../lib/helpers/files_handler'
+
 describe Student do
   context 'when created' do
-    let(:student) { described_class.new(123, 10, 'MIF', 'Informatika') }
-    let(:student_bad) { described_class.new(-123, -10, 'MIF', 'Informatika') }
+    let(:student) do
+      described_class.new(
+        's_id' => 123,
+        'group' => 10,
+        'faculty' => 'MIF',
+        'study_program' => 'Informatika',
+        'subjects' => %w[Ruby Matematika],
+        'active' => 1,
+        'mode' => 'Dieninis'
+      )
+    end
+    let(:student_bad) do
+      described_class.new(
+        's_id' => -123,
+        'group' => -10,
+        'faculty' => 'MIF',
+        'study_program' => 'Informatika',
+        'subjects' => %w[Ruby Matematika],
+        'active' => 1,
+        'mode' => 'Dieninis'
+      )
+    end
 
     it 'initial student number was set successful' do
+      expect(student.s_id).to eq 123
+    end
+
+    it 'initial student number was changed successful' do
+      student.change_sid(100)
+      expect(student.s_id).to eq 100
+    end
+
+    it 'initial student number was changed unsuccessful' do
+      student.change_sid(-100)
       expect(student.s_id).to eq 123
     end
 
