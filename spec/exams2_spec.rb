77ef932eef9792b688da3b8cdb9b01ef106c0a7d
arnasRad <arnasrad@gmail.com>
2018-10-30 18:38:40 +0200

    user, exams specs update

--- spec/exams2_spec.rb
+++ spec/exams2_spec.rb
@@ -23,5 +23,25 @@ describe Exams do
       regex = /((([0-1][0-9])|([2][0-3])):([0-5][0-9]))$/
       expect(!temp_exams2.date_time[regex].nil?).to eq true
     end
+
+    it 'date_month is of type String' do
+      expect(temp_exams2.date_month).to be_instance_of(String)
+    end
+
+    it 'date_month is not of type Integer' do
+      expect(temp_exams2.date_month).not_to be_instance_of(Integer)
+    end
+
+    it 'date_day is of type String' do
+      expect(temp_exams2.date_day).to be_instance_of(String)
+    end
+
+    it 'date_day is not of type Integer' do
+      expect(temp_exams2.date_day).not_to be_instance_of(Integer)
+    end
+
+    it 'date_month is in correct range 2' do
+      expect(temp_exams2.date_month).not_to be_within(13).of(100)
+    end
   end
 end
