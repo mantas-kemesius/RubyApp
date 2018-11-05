f6bf7247691cc83d2f51ee64218cb28d360c9147
mEimantas <43536171+mEimantas@users.noreply.github.com>
2018-11-05 18:02:37 +0200

    fixing

--- spec/teacher_spec.rb
+++ spec/teacher_spec.rb
@@ -5,22 +5,25 @@ describe Teacher do
       described_class.new('t1610179', 'Vilniaus universitetas', 'mif')
     end
 
-    it 'has no subjects' do
-      expect(teacher.subjects).to eq []
+    it 'ID set correct' do
+      expect(teacher.id).to eq 't1610179'
     end
-    it 'can enter grade for student' do
-      expect(teacher.enter_grade?).to eq true
+    it 'university set correct' do
+      expect(teacher.university).to eq 'Vilniaus universitetas'
     end
-    it 'can edit grade for student' do
-      expect(teacher.edit_grade?).to eq true
+    it 'faculty was set correct' do
+      expect(teacher.faculty).to eq 'mif'
     end
-    it 'can delete grade for student' do
-      expect(teacher.delete_grade?).to eq true
+    it 'faculty set correct' do
+      expect(teacher.on_vacation).to eq false
+    end
+    it 'id number is correct format' do
+      regex = /[t]\d{7}$/
+      expect(!teacher.id[regex].nil?).to eq true
     end
     it 'teacher is on vacation' do
-      expect { teacher.goes_on_vacation(true) }
-        .to change { teacher.on_vacation }
-        .from(false).to(true)
+      teacher.goes_on_vacation(true)
+      expect(teacher.on_vacation).to eq true
     end
   end
 end
