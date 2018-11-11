b1f5710ac3354a150b088aa2bc3fafd8f5081fc5
tomasmacikas <macikastomas@gmail.com>
2018-11-11 20:11:44 +0200

    teacher class update

---
+++ ConsoleApp/lib/model/teacher_window.rb
@@ -0,0 +1,56 @@
+require_relative '../../lib/helpers/files_handler'
+require 'pp'
+
+# All teachers are placed here
+class TeacherWindow
+  attr_reader :teachers
+
+  def initialize
+    @teachers = []
+  end
+
+  def add_teacher(teacher)
+    teachers << teacher
+  end
+
+  def delete_teacher(position)
+    teachers.delete_at(position)
+  end
+
+  def delete_by_username(username)
+    teachers.each do |item|
+      if item.username.eql?(username)
+        teachers.delete(item)
+        delete_by_username(username)
+      end
+    end
+  end
+
+  def print_teachers
+    teachers.each(&:print_teacher)
+  end
+
+  # :reek:FeatureEnvy
+  def save_teachers(path)
+    file = FilesHandler.new(path)
+    data = []
+    teachers.each do |teacher|
+      data[data.length] = {
+        'username' => teacher.username, 'university' => teacher.university,
+        'faculty' => teacher.faculty
+      }
+      file.save_data('Teachers' => data)
+    end
+  end
+
+  # :reek:FeatureEnvy
+  def load_teachers(path)
+    file = FilesHandler.new(path)
+    info = file.load_data.fetch('Teachers')
+    info.each do |teacher|
+      add_teacher(Teacher.new(teacher.fetch('username'),
+                              teacher.fetch('university'),
+                              teacher.fetch('faculty')))
+    end
+  end
+end
