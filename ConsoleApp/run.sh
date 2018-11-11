1f21bbe6d6a0e614695da7fe9fabe0bbab7d3628
tomasmacikas <macikastomas@gmail.com>
2018-11-11 21:26:24 +0200

    json file fault

--- ConsoleApp/run.sh
+++ ConsoleApp/run.sh
@@ -2,7 +2,7 @@
 
 bundle install
 
-bundle exec mutant --include lib --use rspec LoginController Teacher Subject SubjectWindow Student User Email SubjectInfo FilesHandler Schedule Exams Grades NotificationWindow Notification
+bundle exec mutant --include lib --use rspec LoginController Teacher TeacherWindow Subject SubjectWindow Student User Email SubjectInfo FilesHandler Schedule Exams Grades NotificationWindow Notification
 bundle exec rspec
 bundle exec rubocop
 bundle exec reek
