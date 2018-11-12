ae882b8203c7a2c557539249d843f70b5d0980a6
mEimantas <43536171+mEimantas@users.noreply.github.com>
2018-11-12 13:15:32 +0200

    Update be_correct_notification.rb

--- ConsoleApp/spec/custom matchers/be_correct_notification.rb
+++ ConsoleApp/spec/custom matchers/be_correct_notification.rb
@@ -1,10 +1,10 @@
 RSpec.configure do
   RSpec::Matchers.define :be_correct_notification do |title, text|
     match do |notification|
-      notification.title.eql(title)&&
-      notification.text.eql(text)
+      notification.title.eql(title) &&
+        notification.text.eql(text)
     end
-    failure_message do |title, text|
+    failure_message do
       " #{title} #{text} is not correct "
     end
   end
