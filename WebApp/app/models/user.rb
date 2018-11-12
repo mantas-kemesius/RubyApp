2770147a1d66939782decd63946b440ffc450565
mantas.kemesius <mantas.kemesius@gmail.com>
2018-11-12 21:26:11 +0200

    update user validation

--- WebApp/app/models/user.rb
+++ WebApp/app/models/user.rb
@@ -1,9 +1,11 @@
 # User model
 class User < ActiveRecord::Base
   validates_uniqueness_of :email, message: "your email should be unique"
+  validates_uniqueness_of :username, message: "your username should be unique"
   validates_length_of :name, maximum: 40, message: "name maximum length 40 characters"
   validates_length_of :lastname, maximum: 40, message: "lastname maximum length 40 characters"
   validates_length_of :email, maximum: 60, message: "email maximum length 40 characters"
+  validates_length_of :username, maximum: 30, message: "username maximum length 40 characters"
   validates_length_of :university, maximum: 60, message: "university maximum length 40 characters"
   validates :age, inclusion: 1...100
 end
