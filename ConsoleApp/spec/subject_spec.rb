688878d065ae5f197e3cb6d2626e9cdd18eeed2c
mEimantas <43536171+mEimantas@users.noreply.github.com>
2018-11-13 17:20:55 +0200

    Update subject_spec.rb

--- ConsoleApp/spec/subject_spec.rb
+++ ConsoleApp/spec/subject_spec.rb
@@ -1,20 +1,53 @@
 require 'spec_helper'
 describe Subject do
   context 'when created' do
-    let(:temp_subject) do
+    let(:item) do
       described_class.new('Ruby Programming', 'Agile Programming Ruby',
                           '5', 't1610179')
     end
 
-    it 'Subject name set correct' do
-      expect(temp_subject.title).to eq 'Ruby Programming'
+    it 'Coordinating teacher ID correct format' do
+      regex = /[t]\d{7}$/
+      expect(!item.teacher[regex].nil?).to eq true
     end
-    it 'Subject description set correct' do
-      expect(temp_subject.description).to eq 'Agile Programming Ruby'
+    it 'Coordinating teacher set correct' do
+      expect(item.teacher).to eq 't1610179'
     end
-    it 'Credit number correct format' do
-      regex = /^(3|4|5|6|7|8|9|10|11|12|13|14|15){1}$/
-      expect(!temp_subject.credits[regex].nil?).to eq true
+    it 'Credit number set correct' do
+      expect(item.credits).to eq '5'
+    end
+    it 'subject printed' do
+      s = item.title + "\n" + item.description + "\n" + item.credits + "\n"
+      s += item.teacher
+      s += "\n" + '__________________________________________________' + "\n"
+      expect { item.print_subject }.to output(s).to_stdout
+    end
+    context 'when info change' do
+      let(:item) do
+        described_class.new('Ruby Programming', 'Agile Programming Ruby',
+                            '5', 't1610179')
+      end
+
+      it 'title changed correctly' do
+        expect { item.change_title('NewTitle') }
+          .to change(item, :title)
+          .from('Ruby Programming').to('NewTitle')
+      end
+      it 'description changed correctly' do
+        expect { item.change_description('newDesc') }
+          .to change(item, :description)
+          .from('Agile Programming Ruby').to('newDesc')
+      end
+      it 'credits changed correctly' do
+        expect { item.change_credits('10') }
+          .to change(item, :credits)
+          .from('5').to('10')
+      end
+      it 'teacher change correctly' do
+        expect { item.change_teacher('Plikusas') }
+          .to change(item, :teacher)
+          .from('t1610179').to('Plikusas')
+      end
     end
   end
 end
