f495cb6744595e13c87b73ed430365c8138deb59
tomasmacikas <macikastomas@gmail.com>
2018-11-18 13:19:58 +0200

    notification load update

--- ConsoleApp/spec/notification_spec.rb
+++ ConsoleApp/spec/notification_spec.rb
@@ -28,4 +28,41 @@ RSpec.describe(Notification) do
       expect { note.print_notification }.to output(s).to_stdout
     end
   end
+
+  context 'when compared' do
+    let(:note) do
+      described_class.new('2018-28-10', 'First', 'Hello', 'Admin')
+    end
+    let(:same_note) do
+      described_class.new('2018-28-10', 'First', 'Hello', 'Admin')
+    end
+    let(:diff_date) do
+      described_class.new('2018-28-11', 'First', 'Hello', 'Admin')
+    end
+    let(:diff_title) do
+      described_class.new('2018-28-10', 'Second', 'Hello', 'Admin')
+    end
+    let(:diff_text) do
+      described_class.new('2018-28-10', 'First', 'Hi', 'Admin')
+    end
+    let(:diff_sender) do
+      described_class.new('2018-28-10', 'First', 'Hello', 'Tomas')
+    end
+
+    it 'is not same when date different' do
+      expect(note == diff_date).to be false
+    end
+    it 'is not same when title different' do
+      expect(note == diff_title).to be false
+    end
+    it 'is not same when text different' do
+      expect(note == diff_text).to be false
+    end
+    it 'is not same when sender different' do
+      expect(note == diff_sender).to be false
+    end
+    it 'is same' do
+      expect(note == same_note).to be true
+    end
+  end
 end
