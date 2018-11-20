75c51aad817a39946c793f2c2ee10f1cc99009ee
tomasmacikas <macikastomas@gmail.com>
2018-11-20 16:56:39 +0200

    rspec fix

--- WebApp/app/models/notification.rb
+++ WebApp/app/models/notification.rb
@@ -3,7 +3,13 @@ class Notification < ApplicationRecord
   validates :text, presence: true
   # belongs_to :teacher
 
-  def mutant_should_fail
-        notifications = []
+  def print_notification
+    str = "\n" + title + "\n" + text + "\n"
+    str += '____________________________________________________' + "\n"
+    print str
+  end
+  def change_title(newTitle)
+    @title = newTitle
+    self.save
   end
 end
--- WebApp/spec/notification_spec.rb
+++ WebApp/spec/notification_spec.rb
@@ -1,31 +1,77 @@
-require_relative 'spec_helper'
+require_relative 'rails_helper'
 
-RSpec.describe(Notification) do
-  context 'object created' do
+RSpec.describe Notification, :type => :model do
+  context 'when object created' do
 
-    it 'should have title and text attributes' do
+    it 'have title and text attributes' do
       note = described_class.new(title: 'First', text: 'Hello')
       expect(note).to have_attributes(title: 'First', text: 'Hello')
     end
 
-    it 'should be valid' do
+    it 'is valid' do
       note = described_class.new(title: 'First', text: 'Hello')
       expect(note).to be_valid
     end
 
-    # it 'title set correctly' do
-    #   expect(notification).to eq 'First'
-    # end
-    # it 'text set correctly' do
-    #   expect(note.text).to eq 'Hello'
-    # end
-    # it 'sender set correctly' do
-    #   expect(note.sender).to eq 'Admin'
-    # end
-    # it 'notification printed' do
-    #   s = note.date + "\n" + note.title + "\n" + note.text + "\n" + note.sender
-    #   s += "\n" + '____________________________________________________' + "\n"
-    #   expect { note.print_notification }.to output(s).to_stdout
-    # end
+    it 'have title' do
+      note = described_class.new(title: '', text: 'Hello')
+      expect(note).not_to be_valid
+    end
+
+    it 'have text' do
+      note = described_class.new(title: 'First', text: '')
+      expect(note).not_to be_valid
+    end
+  end
+  context 'when added to database' do
+    let(:note) do
+      described_class.new(title: 'First', text: 'Hello')
+    end
+    let(:add) do
+      note.save
+    end
+    let(:delete) do
+      notification = described_class.last
+      notification.destroy
+    end
+    it 'title set correctly' do
+      add
+      notification = Notification.last
+      expect(notification.title).to eq note.title
+      delete
+    end
+
+    it 'text set correctly' do
+      add
+      notification = Notification.last
+      expect(notification.text).to eq note.text
+      delete
+    end
+
+    it 'notification printed' do
+      note = described_class.new(title: 'First', text: '')
+      s = "\n" + note.title + "\n" + note.text + "\n"
+      s += '____________________________________________________' + "\n"
+      expect { note.print_notification }.to output(s).to_stdout
+    end
+  end
+  context 'when deleted from database' do
+    let(:note) do
+      described_class.new(title: 'First', text: 'Hello')
+    end
+    let(:add) do
+      note.save
+    end
+    let(:delete) do
+      notification = described_class.last
+      notification.destroy
+    end
+
+    it 'does not exist' do
+      notification = described_class.last
+      add
+      delete
+      expect(note).not_to eq notification
+    end
   end
 end
