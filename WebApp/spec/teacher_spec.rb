a3c7ae34dc668d2e42764eba497102802184e03d
tomasmacikas <macikastomas@gmail.com>
2018-11-29 21:05:26 +0200

    fixtures example

--- WebApp/spec/teacher_spec.rb
+++ WebApp/spec/teacher_spec.rb
@@ -1,6 +1,12 @@
 require_relative 'spec_helper'
-describe Teacher do
-  context 'when created' do
+RSpec.describe Teacher, :type => :model do
+  fixtures :teachers
+  context 'fixtures loaded' do
+
+    it 'first object loaded' do
+      teachers(:Tomas)
+      expect(teachers(:Tomas).name).to eq('Tomas')
+    end
     # it 'created successfully' do
     #   teacher = described_class.new({:name => 'Tom', :surname => 'Mac'})
     #   expect(teacher).to have_attributes(:name => 'Tom', :surname => 'Mac')
