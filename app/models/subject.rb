d77ebdc7aec46907ffd185472d7d2d7ce9aba9fd
mEimantas <43536171+mEimantas@users.noreply.github.com>
2018-12-03 18:38:29 +0200

    Update subject.rb

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -4,4 +4,16 @@
 class Subject < ApplicationRecord
   validates_length_of :name, maximum: 40,
                              message: 'name maximum length 40 characters'
+  validates :time, presence: true
+  validates :teacher_id, presence: true
+  belongs_to :teacher
+
+  def self.add(name, time, teacher)
+    create(name: name, time: time, teacher_id: teacher.id)
+  end
+
+  def self.del(teacher)
+    subject = find_by(teacher_id: teacher)
+    destroy(subject.id)
+  end
 end
