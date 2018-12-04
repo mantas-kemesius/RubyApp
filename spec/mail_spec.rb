ee41c52794d900156f2f694803816c11fe89a97c
mEimantas <43536171+mEimantas@users.noreply.github.com>
2018-12-04 12:48:46 +0200

    Update mail_spec.rb

--- spec/mail_spec.rb
+++ spec/mail_spec.rb
@@ -8,55 +8,55 @@ RSpec.describe Mail, type: :model do
       expect(Mail.count).to eq mails.size
     end
     it 'has one after adding one' do
-      user1 = User.create!(email: 'asd2', age: 1)
-      user2 = User.create!(email: 'asd', age: 2)
-      Mail.create!(to: user1.id, from: user2.id, title: '8:00', message: 'L')
+      u1 = User.create!(email: 'asd2', age: 1)
+      u2 = User.create!(email: 'asd', age: 2)
+      Mail.create!(to_id: u1.id, from_id: u2.id, title: '8', message: 'L')
       expect(Mail.count).to eq mails.size + 1
     end
     it 'is in database' do
-      user1 = User.create!(email: 'asd2', age: 1)
-      user2 = User.create!(email: 'asd', age: 2)
-      em = Mail.create!(to: user1.id, from: user2.id, title: '8', message: 'L')
+      u1 = User.create!(email: 'asd2', age: 1)
+      u2 = User.create!(email: 'asd', age: 2)
+      em = Mail.create!(to_id: u1.id, from_id: u2.id, title: '8', message: 'L')
 
       expect(Mail.find(em.id)).to be_present
     end
     it 'changed to is in database' do
-      user1 = User.create!(email: 'asd2', age: 1)
-      user2 = User.create!(email: 'asd', age: 2)
-      em = Mail.create!(to: user1.id, from: user2.id, title: '8', message: 'L')
-      Mail.update_to_emails(user2.id)
-      expect(Mail.find(em.id).to).to eq user2.id
+      u1 = User.create!(email: 'asd2', age: 1)
+      u2 = User.create!(email: 'asd', age: 2)
+      em = Mail.create!(to_id: u1.id, from_id: u2.id, title: '8', message: 'L')
+      Mail.update_to_emails(u2.id)
+      expect(Mail.find(em.id).to_id).to eq u2.id
     end
     it 'changed from is in database' do
-      user1 = User.create!(email: 'asd2', age: 1)
-      user2 = User.create!(email: 'asd', age: 2)
-      em = Mail.create!(to: user1.id, from: user2.id, title: '8', message: 'L')
-      Mail.update_from_emails(user1.id)
-      expect(Mail.find(em.id).from).to eq user1.id
+      u1 = User.create!(email: 'asd2', age: 1)
+      u2 = User.create!(email: 'asd', age: 2)
+      em = Mail.create!(to_id: u1.id, from_id: u2.id, title: '8', message: 'L')
+      Mail.update_from_emails(u1.id)
+      expect(Mail.find(em.id).from_id).to eq u1.id
     end
     it 'changed title is in database' do
-      user1 = User.create!(email: 'asd2', age: 1)
-      user2 = User.create!(email: 'asd', age: 2)
-      em = Mail.create!(to: user1.id, from: user2.id, title: '8', message: 'L')
+      u1 = User.create!(email: 'asd2', age: 1)
+      u2 = User.create!(email: 'asd', age: 2)
+      em = Mail.create!(to_id: u1.id, from_id: u2.id, title: '8', message: 'L')
       Mail.update_title('naujas')
 
       expect(Mail.find(em.id).title).to eq 'naujas'
     end
     it 'changed message is in database' do
-      user1 = User.create!(email: 'asd2', age: 1)
-      user2 = User.create!(email: 'asd', age: 2)
-      em = Mail.create!(to: user1.id, from: user2.id, title: '7', message: 'L')
+      u1 = User.create!(email: 'asd2', age: 1)
+      u2 = User.create!(email: 'asd', age: 2)
+      em = Mail.create!(to_id: u1.id, from_id: u2.id, title: '8', message: 'L')
       Mail.update_message('naujas2')
 
       expect(Mail.find(em.id).message).to eq 'naujas2'
     end
 
     it 'added successfully' do
-      user1 = User.create!(email: 'asd2', age: 1)
-      user2 = User.create!(email: 'asd', age: 2)
+      u1 = User.create!(email: 'asd2', age: 1)
+      u2 = User.create!(email: 'asd', age: 2)
 
-      Mail.add(user1.id, user2.id, 'a', 't')
-      bo = Mail.exists?(from: user1.id, to: user2.id, title: 'a', message: 't')
+      Mail.add(u1.id, u2.id, 'a', 't')
+      bo = Mail.exists?(from_id: u1.id, to_id: u2.id, title: 'a', message: 't')
       expect(bo).to be true
     end
   end
@@ -78,7 +78,7 @@ RSpec.describe Mail, type: :model do
     it 'deletes item by user' do
       mail = mails(:Bronius)
 
-      Mail.del(mail.from)
+      Mail.del(mail.from_id)
       expect(Mail.exists?(mail.id)).to be false
     end
   end
