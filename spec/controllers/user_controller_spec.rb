3382efea0e1b5bc15faf6681a2831c304eecad2d
juozas <j.natkevicius@gmail.com>
2018-12-16 23:55:37 +0200

    fetchall rename

---
+++ spec/controllers/user_controller_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe UserController, type: :controller do
+  # let(:UserControl) do
+  #   UserControl = described_class
+  #   allow_any_instance_of(UserControl).to receive(:login).and_return(true)
+  #   UserControl
+  # end
+  #
+  # let(:login_hash) do
+  #   { user: { email: 'test@gmail.com', password: 'p4ssw1rd' } }
+  # end
+  #
+  # it 'signs user in if credentials are valid' do
+  #   hash1 = { 'user' => { 'email' => 'samuolaitis@gmail.com',
+  #                         'password' => 'slaptazodisxd' } }
+  #   expect_any_instance_of(UserControl).to receive(:proceed_login)
+  #   post :proceed_login, params: hash1
+  # end
+end
