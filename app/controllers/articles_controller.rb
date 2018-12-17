d59df28145208c50e34524eae7ce7a40af98001b
mEimantas <43536171+mEimantas@users.noreply.github.com>
2018-12-18 00:57:26 +0200

    mutant

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,15 +10,15 @@ class ArticlesController < ApplicationController
     @article = params[:article]
   end
 
-  def update
-    @article = Article.find params[:id]
-
-    if @article.update(article_params)
-      redirect_to @article
-    else
-      render 'edit'
-    end
-  end
+#  def update
+#    @article = Article.find params[:id]
+#
+#    if @article.update(article_params)
+#      redirect_to @article
+#    else
+#      render 'edit'
+#    end
+#  end
 
   def create
     @article = Article.new(article_params)
