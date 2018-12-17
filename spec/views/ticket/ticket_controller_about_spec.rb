218e7fc16e8711fb70e3c5bc0382509a8e9b9968
Mantas Kemėšius <mantaskemesius@Mantass-Air.lan>
2018-12-17 23:34:10 +0200

    add more views tests

--- spec/views/ticket/ticket_controller_about_spec.rb
+++ spec/views/ticket/ticket_controller_about_spec.rb
@@ -7,21 +7,31 @@ RSpec.describe TicketController, type: :controller do
 
   it 'about view have h1 with words all tickets' do
     get :about
-    expect(response.body.match?('<h1>About ticket</h1>')).to be true
+    expect(response.body.match?('<h1>About ticket</h1>')).to eq true
   end
 
   it 'about view have form' do
     get :about
-    expect(response.body.match?('<form action = "/ticket/about" method = "post">')).to be true
+    expect(response.body.match?('<form action = "/ticket/about" method = "post">')).to eq true
   end
 
   it 'about view have input' do
     get :about
-    expect(response.body.match?('<input value = "about"/>')).to be true
+    expect(response.body.match?('<input value = "about"/>')).to eq true
   end
 
   it 'about view have button' do
     get :about
-    expect(response.body.match?('<button>About</button>')).to be true
+    expect(response.body.match?('<button>About</button>')).to eq true
+  end
+
+  it 'index view have javascript lib files' do
+    get :about
+    expect(response.body.match?('<script src="js/vendor.js"></script>')).to eq true
+  end
+
+  it 'index view have javascript files' do
+    get :about
+    expect(response.body.match?('<script src="js/app.js"></script>')).to eq true
   end
 end
--- spec/views/ticket/ticket_controller_create_spec.rb
+++ spec/views/ticket/ticket_controller_create_spec.rb
@@ -7,26 +7,40 @@ RSpec.describe TicketController, type: :controller do
 
   it 'create view have form' do
     get :create
-    expect(response.body.match?('form')).to be true
+    expect(response.body.match?('form')).to eq true
   end
 
   it 'create view have submit button' do
     get :create
-    expect(response.body.match?('submit')).to be true
+    expect(
+        response.body.match?('submit')
+    ).to eq true
   end
 
   it 'create view have ticket input' do
     get :create
-    expect(response.body.match?('input')).to be true
+    expect(response.body.match?('input')).to eq true
   end
 
   it 'create view form action is to /ticket/create' do
     get :create
-    expect(response.body.match?('action="/ticket/create"')).to be true
+    expect(
+        response.body.match?('action="/ticket/create"')
+    ).to eq true
   end
 
   it 'create view submit name should be add' do
     get :create
-    expect(response.body.match?(`value="Add"`)).to be true
+    expect(response.body.match?(`value="Add"`)).to eq true
+  end
+
+  it 'index view have javascript lib files' do
+    get :index
+    expect(response.body.match?('<script src="js/vendor.js"></script>')).to eq true
+  end
+
+  it 'index view have javascript files' do
+    get :index
+    expect(response.body.match?('<script src="js/app.js"></script>')).to eq true
   end
 end
