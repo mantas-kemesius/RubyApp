79b67a9f860c5d7d4fe92f0320e8afd93be9dfff
Mantas Kemėšius <mantaskemesius@Mantass-Air.lan>
2018-12-17 21:02:08 +0200

    add ticets

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 bundle install
 
-bundle exec mutant --include app/models --use rspec TicketController UserController#modify_age UserController#delete HomeController TicketController#create TicketController#delete
+bundle exec mutant --include app/models --use rspec TicketController HomeController UserController#modify_age UserController#delete
 bundle exec rspec
 bundle exec rubocop
 bundle exec reek
