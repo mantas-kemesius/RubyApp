5952391a5f6b3369e76f027b8b65e506ad771643
mEimantas <43536171+mEimantas@users.noreply.github.com>
2018-12-18 15:07:38 +0200

    Update ssubject_controller_fetch_all_spec.rb

--- spec/views/ssubject/ssubject_controller_fetch_all_spec.rb
+++ spec/views/ssubject/ssubject_controller_fetch_all_spec.rb
@@ -20,12 +20,12 @@ RSpec.describe SsubjectsController, type: :controller do
   it 'expected to have student_id: 20' do
     post :create, params: { ssubject: { student_id: 20, subject_id: 10 } }
     get :fetch_all
-    expect(response.body.match?('subject_id')).to eq true
+    expect(response.body.match?('\"student_id\":20')).to eq true
   end
 
   it 'expected to have subject_id: 10' do
     post :create, params: { ssubject: { student_id: 20, subject_id: 10 } }
     get :fetch_all
-    expect(response.body.match?('subject_id')).to eq true
+    expect(response.body.match?('\"subject_id\":10')).to eq true
   end
 end
