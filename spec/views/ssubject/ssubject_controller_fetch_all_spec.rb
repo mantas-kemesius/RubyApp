d4b31712955b26b75ea04e145769e6a6def11751
arnas <arnasrad@gmail.com>
2018-12-18 11:39:02 +0200

    rubocop fix 3

--- spec/views/ssubject/ssubject_controller_fetch_all_spec.rb
+++ spec/views/ssubject/ssubject_controller_fetch_all_spec.rb
@@ -5,25 +5,25 @@ require 'rails_helper'
 RSpec.describe SsubjectsController, type: :controller do
   render_views
 
-  it 'should have stundet_id key' do
+  it 'expected to have stundet_id key' do
     post :create, params: { ssubject: { student_id: 20, subject_id: 10 } }
     get :fetch_all
     expect(response.body.match?('student_id')).to eq true
   end
 
-  it 'should have subject_id key' do
+  it 'expected to have subject_id key' do
     post :create, params: { ssubject: { student_id: 20, subject_id: 10 } }
     get :fetch_all
     expect(response.body.match?('subject_id')).to eq true
   end
 
-  it 'should have student_id: 20' do
+  it 'expected to have student_id: 20' do
     post :create, params: { ssubject: { student_id: 20, subject_id: 10 } }
     get :fetch_all
     expect(response.body.match?('subject_id')).to eq true
   end
 
-  it 'should have subject_id: 10' do
+  it 'expected to have subject_id: 10' do
     post :create, params: { ssubject: { student_id: 20, subject_id: 10 } }
     get :fetch_all
     expect(response.body.match?('subject_id')).to eq true
