0ed274d31c1a401a77d07a5fb1be12ef42840db4
mEimantas <43536171+mEimantas@users.noreply.github.com>
2019-01-04 13:16:52 +0200

    Update user_controller_render_spec.rb

--- spec/views/user/user_controller_render_spec.rb
+++ spec/views/user/user_controller_render_spec.rb
@@ -5,20 +5,24 @@ require 'rails_helper'
 RSpec.describe UserController, type: :controller do
   render_views
 
-  let(:new_hash2) do
+  let(:new_hash) do
     { 'user' => { 'name' => 'testas', 'lastname' => 'testaitis',
-                  'email' => 'tomas.tomaitis@gmail.com',
+                  'email' => 'testas@testaitis.com',
                   'password' => 'paswordukas',
-                  'age' => 23, 'role' => 'TEACHER' } }
+                  'age' => '28', 'role' => 'TEACHER' } }
   end
 
-  before { post :register, params: new_hash2 }
-
-  it 'when incorrect resder status 404' do
+  it 'has status 404 when failed' do
+    subjectspy = instance_spy('subjectspy', save: false)
+    allow(User).to receive(:new).and_return(subjectspy)
+    post :register, params: new_hash
     expect(response.status).to eq 404
   end
 
-  it 'when incorrect resder body Failed' do
+  it 'has body failed when unsuccesful' do
+    subjectspy = instance_spy('subjectspy', save: false)
+    allow(User).to receive(:new).and_return(subjectspy)
+    post :register, params: new_hash
     expect(response.body.match?('Failed!')).to eq true
   end
 end
